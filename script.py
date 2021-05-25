aef1fc7331cba62b7a2c81d4971f733e3a767fab
reginaldo <reginaldo.oliveira@gamersclub.com.br>
2021-05-25 19:06:28 -0300

    first commit

---
+++ script.py
@@ -0,0 +1,31 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+from getpass import getpass
+
+site = input("Enter a url:\n")
+
+folder = input("Enter a output folder:\n")
+
+user = input("Enter a user:\n")
+
+password = getpass("password: ")
+
+response = requests.get(site, auth=(user, password))
+
+soup = BeautifulSoup(response.text, 'html.parser')
+img_tags = soup.find_all('img')
+
+urls = [img['src'] for img in img_tags]
+
+for url in urls:
+    filename = re.search(r'/([\w_-]+[.](jpg|gif|png))$', url)
+    if not filename:
+         print("Regex didn't match with the url: {}".format(url))
+         continue
+    newfile = folder + filename.group(1)
+    with open(newfile, 'wb') as f:
+        if 'http' not in url:
+            url = '{}{}'.format(site, url)
+        response = requests.get(url)
+        f.write(response.content)
